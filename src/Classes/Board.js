8fab162e63810f7eeb5242ff581e2564728f6129
MaximeBienassis <maxime.bienassis@edu.ece.fr>
2020-10-28 11:55:38 +0100

    Test Board and Gameboard

--- src/Classes/Board.js
+++ src/Classes/Board.js
@@ -1,6 +1,6 @@
 class Board {
     constructor(size){
-        this.table = Array(size).fill(0).map(()=>Array(size).fill(0))
+        this.table = Array(size).fill().map(()=>Array(size).fill('.'))
     }
 
     getTable(){
--- tests/unit/Board.spec.js
+++ tests/unit/Board.spec.js
@@ -4,9 +4,18 @@ describe("Board", () => {
 
     it("should be an array 3x3", () => {
         let newBoard = new Board(3)
-        let boardTable = newBoard.getTable()
-        expect(boardTable).toHaveLength(3)
-        expect(boardTable[0]).toHaveLength(3)
+        let table = newBoard.getTable()
+        expect(table).toHaveLength(3)
+        expect(table[0]).toHaveLength(3)
     })
+    it('should be filled with dots', () => {
+        let newBoard = new Board(3)
+        let table = newBoard.getTable()
+        console.log(table)
+        let count = 0
+        table.forEach(line => count += line.filter(cell => cell === '.').length)
+        expect(count).toEqual(8)
+        
+    });
 
 })
